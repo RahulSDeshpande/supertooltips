6b96991175a5683f3a377866a1e737f3d6eebaaf
Ian Thomas <toxicbakery@gmail.com>
2013-10-22 13:11:48 -0400

    Library cleanup and switch to use of API11+
     - Updated library to API 11 minimum.
     - Removed compat code for < API11

--- library/src/com/haarman/supertooltips/ToolTipView.java
+++ library/src/com/haarman/supertooltips/ToolTipView.java
@@ -18,7 +18,6 @@ package com.haarman.supertooltips;
 import android.content.Context;
 import android.graphics.PorterDuff;
 import android.graphics.Rect;
-import android.os.Build;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -40,314 +39,220 @@ import java.util.List;
  */
 public class ToolTipView extends LinearLayout implements ViewTreeObserver.OnPreDrawListener, View.OnClickListener {
 
-    private ImageView mTopPointerView;
-    private View mTopFrame;
-    private ViewGroup mContentHolder;
-    private TextView mToolTipTV;
-    private View mBottomFrame;
-    private ImageView mBottomPointerView;
-    private View mShadowView;
-    //
-    private ToolTip mToolTip;
-    private View mView;
-    //
-    private boolean mDimensionsKnown;
-    private int mRelativeMasterViewY;
-    //
-    private int mRelativeMasterViewX;
-    private int mWidth;
-    //
-    private OnToolTipViewClickedListener mListener;
+	private ImageView mTopPointerView;
+	private View mTopFrame;
+	private ViewGroup mContentHolder;
+	private TextView mToolTipTV;
+	private View mBottomFrame;
+	private ImageView mBottomPointerView;
+	private View mShadowView;
+	//
+	private ToolTip mToolTip;
+	private View mView;
+	//
+	private boolean mDimensionsKnown;
+	private int mRelativeMasterViewY;
+	//
+	private int mRelativeMasterViewX;
+	private int mWidth;
+	//
+	private OnToolTipViewClickedListener mListener;
 
-    public ToolTipView(Context context) {
-        super(context);
-        init();
-    }
+	public ToolTipView(Context context) {
+		super(context);
 
-    private void init() {
-        setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
-        setOrientation(VERTICAL);
-        LayoutInflater.from(getContext()).inflate(R.layout.tooltip, this, true);
+		setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
+		setOrientation(VERTICAL);
+		LayoutInflater.from(getContext()).inflate(R.layout.tooltip, this, true);
 
-        mTopPointerView = (ImageView) findViewById(R.id.tooltip_pointer_up);
-        mTopFrame = findViewById(R.id.tooltip_topframe);
-        mContentHolder = (ViewGroup) findViewById(R.id.tooltip_contentholder);
-        mToolTipTV = (TextView) findViewById(R.id.tooltip_contenttv);
-        mBottomFrame = findViewById(R.id.tooltip_bottomframe);
-        mBottomPointerView = (ImageView) findViewById(R.id.tooltip_pointer_down);
-        mShadowView = findViewById(R.id.tooltip_shadow);
+		mTopPointerView = (ImageView) findViewById(R.id.tooltip_pointer_up);
+		mTopFrame = findViewById(R.id.tooltip_topframe);
+		mContentHolder = (ViewGroup) findViewById(R.id.tooltip_contentholder);
+		mToolTipTV = (TextView) findViewById(R.id.tooltip_contenttv);
+		mBottomFrame = findViewById(R.id.tooltip_bottomframe);
+		mBottomPointerView = (ImageView) findViewById(R.id.tooltip_pointer_down);
+		mShadowView = findViewById(R.id.tooltip_shadow);
 
-        setOnClickListener(this);
-        getViewTreeObserver().addOnPreDrawListener(this);
-    }
+		setOnClickListener(this);
+		getViewTreeObserver().addOnPreDrawListener(this);
+	}
 
-    @Override
-    public boolean onPreDraw() {
-        getViewTreeObserver().removeOnPreDrawListener(this);
-        mDimensionsKnown = true;
+	@Override
+	public boolean onPreDraw() {
+		getViewTreeObserver().removeOnPreDrawListener(this);
+		mDimensionsKnown = true;
 
-        mWidth = mContentHolder.getWidth();
+		mWidth = mContentHolder.getWidth();
 
-        RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) getLayoutParams();
-        layoutParams.width = mWidth;
-        setLayoutParams(layoutParams);
+		RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) getLayoutParams();
+		layoutParams.width = mWidth;
+		setLayoutParams(layoutParams);
 
-        if (mToolTip != null) {
-            applyToolTipPosition();
-        }
-        return true;
-    }
+		if (mToolTip != null) {
+			applyToolTipPosition();
+		}
+		return true;
+	}
 
-    public void setToolTip(ToolTip toolTip, View view) {
-        mToolTip = toolTip;
-        mView = view;
+	public void setToolTip(ToolTip toolTip, View view) {
+		mToolTip = toolTip;
+		mView = view;
 
-        if (mToolTip.getText() != null) {
-            mToolTipTV.setText(mToolTip.getText());
-        } else if (mToolTip.getTextResId() != 0) {
-            mToolTipTV.setText(mToolTip.getTextResId());
-        }
+		if (mToolTip.getText() != null) {
+			mToolTipTV.setText(mToolTip.getText());
+		} else if (mToolTip.getTextResId() != 0) {
+			mToolTipTV.setText(mToolTip.getTextResId());
+		}
 
-        if (mToolTip.getColor() != 0) {
-            setColor(mToolTip.getColor());
-        }
+		if (mToolTip.getColor() != 0) {
+			setColor(mToolTip.getColor());
+		}
 
-        if (mToolTip.getContentView() != null) {
-            setContentView(mToolTip.getContentView());
-        }
+		if (mToolTip.getContentView() != null) {
+			setContentView(mToolTip.getContentView());
+		}
 
-        if (!mToolTip.getShadow()) {
-            mShadowView.setVisibility(View.GONE);
-        }
+		if (!mToolTip.getShadow()) {
+			mShadowView.setVisibility(View.GONE);
+		}
 
-        if (mDimensionsKnown) {
-            applyToolTipPosition();
-        }
-    }
+		if (mDimensionsKnown) {
+			applyToolTipPosition();
+		}
+	}
 
-    private void applyToolTipPosition() {
-        final int[] masterViewScreenPosition = new int[2];
-        final int[] parentViewScreenPosition = new int[2];
+	private void applyToolTipPosition() {
+		final int[] masterViewScreenPosition = new int[2];
+		final int[] parentViewScreenPosition = new int[2];
 
-        final Rect viewDisplayFrame = new Rect(); // includes decorations (e.g. status bar)
-        mView.getLocationOnScreen(masterViewScreenPosition);
-        mView.getWindowVisibleDisplayFrame(viewDisplayFrame);
-        ((View) getParent()).getLocationOnScreen(parentViewScreenPosition);
+		final Rect viewDisplayFrame = new Rect(); // includes decorations (e.g. status bar)
+		mView.getLocationOnScreen(masterViewScreenPosition);
+		mView.getWindowVisibleDisplayFrame(viewDisplayFrame);
+		((View) getParent()).getLocationOnScreen(parentViewScreenPosition);
 
-        final int masterViewWidth = mView.getWidth();
-        final int masterViewHeight = mView.getHeight();
+		final int masterViewWidth = mView.getWidth();
+		final int masterViewHeight = mView.getHeight();
 
-        mRelativeMasterViewX = masterViewScreenPosition[0] - parentViewScreenPosition[0];
-        mRelativeMasterViewY = masterViewScreenPosition[1] - parentViewScreenPosition[1];
-        final int relativeMasterViewCenterX = mRelativeMasterViewX + masterViewWidth / 2;
+		mRelativeMasterViewX = masterViewScreenPosition[0] - parentViewScreenPosition[0];
+		mRelativeMasterViewY = masterViewScreenPosition[1] - parentViewScreenPosition[1];
+		final int relativeMasterViewCenterX = mRelativeMasterViewX + masterViewWidth / 2;
 
-        float toolTipViewAboveY = mRelativeMasterViewY - getHeight();
-        float toolTipViewBelowY = mRelativeMasterViewY + masterViewHeight;
-        float toolTipViewY;
+		float toolTipViewAboveY = mRelativeMasterViewY - getHeight();
+		float toolTipViewBelowY = mRelativeMasterViewY + masterViewHeight;
+		float toolTipViewY;
 
-        float toolTipViewX = Math.max(0, relativeMasterViewCenterX - mWidth / 2);
-        if (toolTipViewX + mWidth > viewDisplayFrame.right) {
-            toolTipViewX = viewDisplayFrame.right - mWidth;
-        }
+		float toolTipViewX = Math.max(0, relativeMasterViewCenterX - mWidth / 2);
+		if (toolTipViewX + mWidth > viewDisplayFrame.right) {
+			toolTipViewX = viewDisplayFrame.right - mWidth;
+		}
 
-        setX(toolTipViewX);
-        setPointerCenterX(relativeMasterViewCenterX);
+		setX(toolTipViewX);
+		setPointerCenterX(relativeMasterViewCenterX);
 
-        final boolean showBelow = toolTipViewAboveY < 0;
+		final boolean showBelow = toolTipViewAboveY < 0;
 
-        if (Build.VERSION.SDK_INT < 11) {
-            ViewHelper.setAlpha(mTopPointerView, showBelow ? 1 : 0);
-            ViewHelper.setAlpha(mBottomPointerView, showBelow ? 0 : 1);
-        } else {
-            mTopPointerView.setVisibility(showBelow ? VISIBLE : GONE);
-            mBottomPointerView.setVisibility(showBelow ? GONE : VISIBLE);
-        }
+		mTopPointerView.setVisibility(showBelow ? VISIBLE : GONE);
+		mBottomPointerView.setVisibility(showBelow ? GONE : VISIBLE);
 
-        if (showBelow) {
-            toolTipViewY = toolTipViewBelowY;
-        } else {
-            toolTipViewY = toolTipViewAboveY;
-        }
+		toolTipViewY = showBelow?  toolTipViewBelowY : toolTipViewAboveY;
 
-        List<Animator> animators = new ArrayList<Animator>();
+		List<Animator> animators = new ArrayList<Animator>();
 
-        if (mToolTip.getAnimationType() == ToolTip.ANIMATIONTYPE_FROMMASTERVIEW) {
-            animators.add(ObjectAnimator.ofFloat(this, "translationY", mRelativeMasterViewY + mView.getHeight() / 2 - getHeight() / 2, toolTipViewY));
-            animators.add(ObjectAnimator.ofFloat(this, "translationX", mRelativeMasterViewX + mView.getWidth() / 2 - mWidth / 2, toolTipViewX));
-        } else if (mToolTip.getAnimationType() == ToolTip.ANIMATIONTYPE_FROMTOP) {
-            animators.add(ObjectAnimator.ofFloat(this, "translationY", 0, toolTipViewY));
-        }
+		if (mToolTip.getAnimationType() == ToolTip.ANIMATIONTYPE_FROMMASTERVIEW) {
+			animators.add(ObjectAnimator.ofFloat(this, "translationY", mRelativeMasterViewY + mView.getHeight() / 2
+					- getHeight() / 2, toolTipViewY));
+			animators.add(ObjectAnimator.ofFloat(this, "translationX", mRelativeMasterViewX + mView.getWidth() / 2
+					- mWidth / 2, toolTipViewX));
+		} else if (mToolTip.getAnimationType() == ToolTip.ANIMATIONTYPE_FROMTOP) {
+			animators.add(ObjectAnimator.ofFloat(this, "translationY", 0, toolTipViewY));
+		}
 
-        animators.add(ObjectAnimator.ofFloat(this, "scaleX", 0, 1));
-        animators.add(ObjectAnimator.ofFloat(this, "scaleY", 0, 1));
+		animators.add(ObjectAnimator.ofFloat(this, "scaleX", 0, 1));
+		animators.add(ObjectAnimator.ofFloat(this, "scaleY", 0, 1));
 
-        animators.add(ObjectAnimator.ofFloat(this, "alpha", 0, 1));
+		animators.add(ObjectAnimator.ofFloat(this, "alpha", 0, 1));
 
-        AnimatorSet animatorSet = new AnimatorSet();
-        animatorSet.playTogether(animators);
+		AnimatorSet animatorSet = new AnimatorSet();
+		animatorSet.playTogether(animators);
 
-        if (Build.VERSION.SDK_INT < 11) {
-            final float fToolTipViewX = toolTipViewX;
-            final float fToolTipViewY = toolTipViewY;
-            animatorSet.addListener(new Animator.AnimatorListener() {
+		animatorSet.start();
+	}
 
-                @Override
-                public void onAnimationStart(Animator animator) {
-                }
+	public void setPointerCenterX(int pointerCenterX) {
+		int pointerWidth = Math.max(mTopPointerView.getMeasuredWidth(), mBottomPointerView.getMeasuredWidth());
 
-                @Override
-                public void onAnimationEnd(Animator animator) {
-                    RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) getLayoutParams();
-                    params.leftMargin = (int) fToolTipViewX;
-                    params.topMargin = (int) fToolTipViewY;
-                    setX(0);
-                    setY(0);
-                    setLayoutParams(params);
-                }
+		ViewHelper.setX(mTopPointerView, pointerCenterX - pointerWidth / 2 - getX());
+		ViewHelper.setX(mBottomPointerView, pointerCenterX - pointerWidth / 2 - getX());
+	}
 
-                @Override
-                public void onAnimationCancel(Animator animator) {
-                }
+	public void setOnToolTipViewClickedListener(OnToolTipViewClickedListener listener) {
+		mListener = listener;
+	}
 
-                @Override
-                public void onAnimationRepeat(Animator animator) {
-                }
-            });
-        }
+	public void setColor(int color) {
+		mTopPointerView.setColorFilter(color, PorterDuff.Mode.MULTIPLY);
+		mTopFrame.getBackground().setColorFilter(color, PorterDuff.Mode.MULTIPLY);
+		mBottomPointerView.setColorFilter(color, PorterDuff.Mode.MULTIPLY);
+		mBottomFrame.getBackground().setColorFilter(color, PorterDuff.Mode.MULTIPLY);
+		mContentHolder.setBackgroundColor(color);
+	}
 
-        animatorSet.start();
-    }
+	private void setContentView(View view) {
+		mContentHolder.removeAllViews();
+		mContentHolder.addView(view);
+	}
 
-    public void setPointerCenterX(int pointerCenterX) {
-        int pointerWidth = Math.max(mTopPointerView.getMeasuredWidth(), mBottomPointerView.getMeasuredWidth());
+	public void remove() {
+		List<Animator> animators = new ArrayList<Animator>();
+		if (mToolTip.getAnimationType() == ToolTip.ANIMATIONTYPE_FROMMASTERVIEW) {
+			animators.add(ObjectAnimator.ofFloat(this, "translationY", getY(), mRelativeMasterViewY + mView.getHeight()
+					/ 2 - getHeight() / 2));
+			animators.add(ObjectAnimator.ofFloat(this, "translationX", getX(), mRelativeMasterViewX + mView.getWidth()
+					/ 2 - mWidth / 2));
+		} else {
+			animators.add(ObjectAnimator.ofFloat(this, "translationY", getY(), 0));
+		}
 
-        ViewHelper.setX(mTopPointerView, pointerCenterX - pointerWidth / 2 - getX());
-        ViewHelper.setX(mBottomPointerView, pointerCenterX - pointerWidth / 2 - getX());
-    }
+		animators.add(ObjectAnimator.ofFloat(this, "scaleX", 1, 0));
+		animators.add(ObjectAnimator.ofFloat(this, "scaleY", 1, 0));
 
-    public void setOnToolTipViewClickedListener(OnToolTipViewClickedListener listener) {
-        mListener = listener;
-    }
+		animators.add(ObjectAnimator.ofFloat(this, "alpha", 1, 0));
 
-    public void setColor(int color) {
-        mTopPointerView.setColorFilter(color, PorterDuff.Mode.MULTIPLY);
-        mTopFrame.getBackground().setColorFilter(color, PorterDuff.Mode.MULTIPLY);
-        mBottomPointerView.setColorFilter(color, PorterDuff.Mode.MULTIPLY);
-        mBottomFrame.getBackground().setColorFilter(color, PorterDuff.Mode.MULTIPLY);
-        mContentHolder.setBackgroundColor(color);
-    }
+		AnimatorSet animatorSet = new AnimatorSet();
+		animatorSet.playTogether(animators);
+		animatorSet.addListener(new Animator.AnimatorListener() {
+			@Override
+			public void onAnimationStart(Animator animator) {
+			}
 
-    private void setContentView(View view) {
-        mContentHolder.removeAllViews();
-        mContentHolder.addView(view);
-    }
+			@Override
+			public void onAnimationEnd(Animator animator) {
+				if (getParent() != null) {
+					((ViewGroup) getParent()).removeView(ToolTipView.this);
+				}
+			}
 
-    public void remove() {
-        if (Build.VERSION.SDK_INT < 11) {
-            RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) getLayoutParams();
-            setX(params.leftMargin);
-            setY(params.topMargin);
-            params.leftMargin = 0;
-            params.topMargin = 0;
-            setLayoutParams(params);
-        }
+			@Override
+			public void onAnimationCancel(Animator animator) {
+			}
 
-        List<Animator> animators = new ArrayList<Animator>();
-        if (mToolTip.getAnimationType() == ToolTip.ANIMATIONTYPE_FROMMASTERVIEW) {
-            animators.add(ObjectAnimator.ofFloat(this, "translationY", getY(), mRelativeMasterViewY + mView.getHeight() / 2 - getHeight() / 2));
-            animators.add(ObjectAnimator.ofFloat(this, "translationX", getX(), mRelativeMasterViewX + mView.getWidth() / 2 - mWidth / 2));
-        } else {
-            animators.add(ObjectAnimator.ofFloat(this, "translationY", getY(), 0));
-        }
+			@Override
+			public void onAnimationRepeat(Animator animator) {
+			}
+		});
+		animatorSet.start();
+	}
 
-        animators.add(ObjectAnimator.ofFloat(this, "scaleX", 1, 0));
-        animators.add(ObjectAnimator.ofFloat(this, "scaleY", 1, 0));
+	@Override
+	public void onClick(View view) {
+		remove();
 
-        animators.add(ObjectAnimator.ofFloat(this, "alpha", 1, 0));
+		if (mListener != null) {
+			mListener.onToolTipViewClicked(this);
+		}
+	}
 
-        AnimatorSet animatorSet = new AnimatorSet();
-        animatorSet.playTogether(animators);
-        animatorSet.addListener(new Animator.AnimatorListener() {
-            @Override
-            public void onAnimationStart(Animator animator) {
-            }
-
-            @Override
-            public void onAnimationEnd(Animator animator) {
-                if (getParent() != null) {
-                    ((ViewGroup) getParent()).removeView(ToolTipView.this);
-                }
-            }
-
-            @Override
-            public void onAnimationCancel(Animator animator) {
-            }
-
-            @Override
-            public void onAnimationRepeat(Animator animator) {
-            }
-        });
-        animatorSet.start();
-    }
-
-    @Override
-    public void onClick(View view) {
-        remove();
-
-        if (mListener != null) {
-            mListener.onToolTipViewClicked(this);
-        }
-    }
-
-    /**
-     * Convenience method for getting X.
-     */
-    @Override
-    public float getX() {
-        if (Build.VERSION.SDK_INT >= 11) {
-            return super.getX();
-        } else {
-            return ViewHelper.getX(this);
-        }
-    }
-
-    /**
-     * Convenience method for setting X.
-     */
-    @Override
-    public void setX(float x) {
-        if (Build.VERSION.SDK_INT >= 11) {
-            super.setX(x);
-        } else {
-            ViewHelper.setX(this, x);
-        }
-    }
-
-    /**
-     * Convenience method for getting Y.
-     */
-    @Override
-    public float getY() {
-        if (Build.VERSION.SDK_INT >= 11) {
-            return super.getY();
-        } else {
-            return ViewHelper.getY(this);
-        }
-    }
-
-    /**
-     * Convenience method for setting Y.
-     */
-    @Override
-    public void setY(float y) {
-        if (Build.VERSION.SDK_INT >= 11) {
-            super.setY(y);
-        } else {
-            ViewHelper.setY(this, y);
-        }
-    }
-
-    public interface OnToolTipViewClickedListener {
-        public void onToolTipViewClicked(ToolTipView toolTipView);
-    }
+	public interface OnToolTipViewClickedListener {
+		public void onToolTipViewClicked(ToolTipView toolTipView);
+	}
 }
